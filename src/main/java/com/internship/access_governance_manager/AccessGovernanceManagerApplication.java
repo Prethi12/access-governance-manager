736069d38cd2f5a34bcd779b3aac71b0a465519a
Prethi12 <sce22is012@sairamtap.edu.in>
2026-04-25 20:20:46 +0530

    Day 1 & Day 2:Backend work

---
+++ src/main/java/com/internship/access_governance_manager/AccessGovernanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.internship.access_governance_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessGovernanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessGovernanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/access_governance_manager/AccessGovernanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.access_governance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessGovernanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
